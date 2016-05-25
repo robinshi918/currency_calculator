358b07ef7fbbfda167b1c1cf3d4f758def7f5c06
史云 <shiyun@doumi.com>
2016-05-25 10:43:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shiyun/myapplication/CalculatorActivity.java
@@ -0,0 +1,120 @@
+package com.example.shiyun.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    private static final String TAG = CalculatorActivity.class.getSimpleName();
+
+    Button exchangeButton;
+    ImageButton upperButton;
+    EditText upperEdit;
+    ImageButton lowerButton;
+    EditText lowerEdit;
+
+    View.OnClickListener upperChangeCurrencyListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "upperChangeCurrencyListener.onClick");
+        }
+    };
+
+    View.OnClickListener lowerChangeCurrencyListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "lowerChangeCurrencyListener.onClick");
+        }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+        findViews();
+    }
+
+    private void findViews() {
+        //init widgets
+        exchangeButton = (Button) findViewById(R.id.button_exchange);
+        upperButton = (ImageButton) findViewById(R.id.upper_change_currency_button);
+        upperEdit = (EditText) findViewById(R.id.upper_currency_value);
+        lowerButton = (ImageButton) findViewById(R.id.lower_change_currency_button);
+        lowerEdit = (EditText) findViewById(R.id.lower_currency_value);
+
+        // init listeners
+        lowerButton.setOnClickListener(lowerChangeCurrencyListener);
+        upperButton.setOnClickListener(upperChangeCurrencyListener);
+
+        lowerEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lowerEdit.setText("");
+            }
+        });
+
+        upperEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upperEdit.setText("");
+            }
+        });
+
+        upperEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (upperEdit.hasFocus()) {
+                    lowerEdit.setText(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        lowerEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lowerEdit.setText("");
+            }
+        });
+
+        lowerEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (lowerEdit.hasFocus()) {
+                    upperEdit.setText(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/shiyun/myapplication/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.shiyun.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+//@JavascriptInterface
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.jumpButton);
+        webView = (WebView) findViewById(R.id.web_view);
+
+        if (button1 != null) {
+            button1.setOnClickListener(firstButtonListener);
+        }
+
+        if (button2 != null) {
+            button2.setOnClickListener(toCalculatorButtonListener);
+        }
+
+        if (webView != null) {
+            webView.getSettings().setJavaScriptEnabled(true);
+            String path = "file:///android_asset/html/hello.html";
+            webView.addJavascriptInterface(this, "wst");
+            webView.loadUrl(path);
+
+            try {
+                String[] list = getBaseContext().getAssets().list("html");
+                for (String entry: list) {
+                    Log.d(TAG, "asset entry: " + entry);
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    View.OnClickListener firstButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "button clicked");
+
+            webView.loadUrl("javascript:javacalljs()");
+        }
+    };
+
+    View.OnClickListener toCalculatorButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    @JavascriptInterface
+    public void startFunction() {
+        Toast.makeText(this, "js调用了java函数", Toast.LENGTH_SHORT).show();
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "js调用了java函数", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void startFunction(String text) {
+        Toast.makeText(MainActivity.this, "js调用了java函数(带参数)" + text, Toast.LENGTH_SHORT).show();
+    }
+}
